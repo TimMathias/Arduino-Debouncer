239680eca2917fa8f0afb3c55b414dc2eace0cc1
TimMathias <67882955+TimMathias@users.noreply.github.com>
2020-08-11 16:56:49 +0100

    Added an ISR. Changed IsDebounced()
    An an interrupt service routine UpdateISR() which updates the status flags.
    Added a polling update method Update() which updates the status flags.
    Changed the behaviour of IsDebounced(). It no longer updates the status flags.

--- debouncer.h
+++ debouncer.h
@@ -7,44 +7,59 @@
 class Debouncer
 {
 private:
-  const int input_pin;
-  bool output_state;
-  bool rise;
-  bool fall;
-  const unsigned long debounce_delay_ms;
-  unsigned long last_ms;
+
+  const int INPUT_PIN;
+  volatile bool is_debounced;
+  volatile bool output_state;
+  volatile bool rise;
+  volatile bool fall;
+  const unsigned long DEBOUNCE_DELAY_ms;
+  volatile unsigned long last_ms;
 
 public:
 
-  Debouncer(const int input_pin, const unsigned long debounce_delay_ms = 50)
+  Debouncer(const int INPUT_PIN, const unsigned long DEBOUNCE_DELAY_ms = 50)
       :
-      input_pin(input_pin),
-      debounce_delay_ms(debounce_delay_ms)
+      INPUT_PIN(INPUT_PIN),
+      DEBOUNCE_DELAY_ms(DEBOUNCE_DELAY_ms)
   {
     this->output_state = false;
   }
 
+  bool IsDebounced() const { return is_debounced; }
   bool Output() const { return output_state; }
   bool Rise() const { return rise; }
   bool Fall() const { return fall; }
 
-  bool IsDebounced()
+  // For use as an Interrupt Service Routine where interrupts are disabled upon calling.
+  void UpdateISR()
   {
-    bool input_state;
-    unsigned long current_ms;
+    bool input_state = digitalRead(INPUT_PIN);
+    unsigned long current_ms = millis();
+    Update_(input_state, current_ms);
+  }
 
-    // Temporarily disable interrupts to ensure an accurate time stamp for the sample.
+  // To be called from a polling loop where interrupts may or may not be enabled.
+  void Update()
+  {
+    // Temporarily disable interrupts to ensure an accurate time stamp for the sample, and that status flags are updated synchronously.
 #ifdef ATOMIC_BLOCK
-    ATOMIC_BLOCK( ATOMIC_RESTORESTATE )
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
     {
 #endif
-      input_state = digitalRead(input_pin);  // No interrupt will occur between here...
-      current_ms = millis();                 // ...and here, ensuring an accurate time stamp for the sample.
+      bool input_state = digitalRead(INPUT_PIN);  // No interrupts will occur between here...
+      unsigned long current_ms = millis();        // ...and here, ensuring an accurate time stamp for the sample.
+      Update_(input_state, current_ms);
 #ifdef ATOMIC_BLOCK
     }
 #endif
+  }
 
-    rise = fall = false;
+private:
+
+  inline void Update_(const int& input_state, unsigned long& current_ms)
+  {
+    is_debounced = rise = fall = false;
 
     // Hysteresis:
     //   If there is no change, reset the debounce timer.
@@ -53,15 +68,14 @@ public:
     {
       last_ms = current_ms;
     }
-    else if ((current_ms - last_ms) >= debounce_delay_ms)
+    else if ((current_ms - last_ms) >= DEBOUNCE_DELAY_ms)
     {
       // Successfully debounced, so update the outputs.
+      is_debounced = true;
       rise = input_state && !output_state;
       fall = !input_state && output_state;
       output_state = input_state;
-      return true;
     }
-    return false;
   }
 };
 #endif /* DEBOUNCER_H */
