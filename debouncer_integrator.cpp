2625e42a2197844fae677258a2c27e83f5070fb0
TimMathias <67882955+TimMathias@users.noreply.github.com>
2021-05-31 19:25:54 +0100

    Add files via upload

---
+++ debouncer_integrator.cpp
@@ -0,0 +1,145 @@
+/*
+
+  debouncer_integrator.cpp
+
+  Debounces an input using integration.
+  Detects rising edges, falling edges and both edges.
+
+  MIT License
+
+  Copyright (c) 2021 Timothy Mathias
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+#ifdef __AVR__
+#include <util/atomic.h>
+#endif
+
+bool DebouncerIntegrator::Output() const
+{
+#ifdef ATOMIC_BLOCK
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+  {
+#endif
+    return output_state;
+#ifdef ATOMIC_BLOCK
+  }
+#endif
+}
+
+bool DebouncerIntegrator::Edge() const
+{
+#ifdef ATOMIC_BLOCK
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+  {
+#endif
+    return edge;
+#ifdef ATOMIC_BLOCK
+  }
+#endif
+}
+
+bool DebouncerIntegrator::Rise() const
+{
+#ifdef ATOMIC_BLOCK
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+  {
+#endif
+    return rise;
+#ifdef ATOMIC_BLOCK
+  }
+#endif
+}
+
+bool DebouncerIntegrator::Fall() const
+{
+#ifdef ATOMIC_BLOCK
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+  {
+#endif
+    return fall;
+#ifdef ATOMIC_BLOCK
+  }
+#endif
+}
+
+// To be called from a polling loop where interrupts may or may not be enabled.
+void DebouncerIntegrator::Update()
+{
+  // Temporarily disable interrupts to ensure an accurate time stamp for the sample, and that status flags are updated synchronously.
+#ifdef ATOMIC_BLOCK
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+  {
+#endif
+    const bool input_state = digitalRead(INPUT_PIN);  // No interrupts will occur between here...
+    const unsigned long current_ms = millis();        // ...and here, ensuring an accurate time stamp for the sample...
+    Update_(input_state, current_ms);                 // ...and here, ensuring flags are updated synchronously.
+#ifdef ATOMIC_BLOCK
+  }
+#endif
+}
+
+// For use as an Interrupt Service Routine where interrupts are disabled upon calling.
+void DebouncerIntegrator::UpdateISR()
+{
+  const bool input_state = digitalRead(INPUT_PIN);
+  const unsigned long current_ms = millis();
+  Update_(input_state, current_ms);
+}
+
+void DebouncerIntegrator::Update_(const bool& input_state, const unsigned long& current_ms)
+{
+  const unsigned long delta_ms = current_ms - previous_ms;
+  previous_ms = current_ms;
+
+  // Integrator:
+  //    If there is no change, the sum tends towards zero.
+  //    Else, the sum tends towards the debounce delay.
+  if (input_state == output_state)
+  {
+    if (sum_ms > delta_ms)  // Avoid going negative (underflow).
+    {
+      sum_ms -= delta_ms;  // Tend towards zero if input equals output.
+    }
+    else
+    {
+      sum_ms = 0;
+    }
+  }
+  else
+  {
+    if (delta_ms < DEBOUNCE_DELAY_ms - sum_ms)  // Avoid overflow.
+    {
+        sum_ms += delta_ms;  // Tend towards DEBOUNCE_DELAY_ms if input and output differ.
+    }
+    else
+    {
+      // Successfully debounced, so reset the sum and update the outputs.
+      sum_ms = 0;
+      rise = input_state && !output_state;
+      fall = !input_state && output_state;
+      edge = rise || fall;
+      output_state = input_state;
+      return;
+    }
+  }
+  edge = rise = fall = false;
+}
---
+++ debouncer_integrator.h
@@ -0,0 +1,79 @@
+/*
+
+  debouncer_integrator.h
+
+  Debounces an input using integration.
+  Detects rising edges, falling edges and both edges.
+
+  MIT License
+
+  Copyright (c) 2021 Timothy Mathias
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+#pragma once
+#ifndef DEBOUNCER_INTEGRATOR_H
+#define DEBOUNCER_INTEGRATOR_H
+
+class DebouncerIntegrator
+{
+private:
+
+  const int INPUT_PIN;
+  volatile bool output_state : 1;
+  volatile bool edge : 1;
+  volatile bool rise : 1;
+  volatile bool fall : 1;
+  const unsigned long DEBOUNCE_DELAY_ms;
+  volatile unsigned long previous_ms;
+  volatile unsigned long sum_ms;
+
+public:
+
+  DebouncerIntegrator(const int INPUT_PIN, const unsigned long DEBOUNCE_DELAY_ms = 50)
+    : INPUT_PIN(INPUT_PIN)
+    , DEBOUNCE_DELAY_ms(DEBOUNCE_DELAY_ms)
+    , output_state(digitalRead(INPUT_PIN))
+    , edge(false)
+    , rise(false)
+    , fall(false)
+    , previous_ms(millis())
+    , sum_ms(0)
+  {
+  }
+
+  bool Output() const;
+  bool Edge() const;
+  bool Rise() const;
+  bool Fall() const;
+
+  // To be called from a polling loop where interrupts may or may not be enabled.
+  void Update();
+
+  // For use as an Interrupt Service Routine where interrupts are disabled upon calling.
+  void UpdateISR();
+
+private:
+
+  void Update_(const bool& input_state, const unsigned long& current_ms);
+};
+
+#endif /* DEBOUNCER_INTEGRATOR_H */
