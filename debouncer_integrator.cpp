2b34d576e6c47e3a2e748c0f88964f463f4b8796
TimMathias <67882955+TimMathias@users.noreply.github.com>
2021-09-11 10:19:28 +0100

    Update debouncer_integrator.cpp

--- debouncer_integrator.cpp
+++ debouncer_integrator.cpp
@@ -34,67 +34,51 @@
 #include <util/atomic.h>
 #endif
 
+#ifndef ATOMIC_BLOCK
+  #define ATOMIC_BLOCK(x)
+#endif
+
 #include "debouncer_integrator.h"
 
 bool DebouncerIntegrator::Output() const
 {
-#ifdef ATOMIC_BLOCK
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
   {
-#endif
     return output_state;
-#ifdef ATOMIC_BLOCK
   }
-#endif
 }
 
 bool DebouncerIntegrator::Edge() const
 {
-#ifdef ATOMIC_BLOCK
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
   {
-#endif
     return edge;
-#ifdef ATOMIC_BLOCK
   }
-#endif
 }
 
 bool DebouncerIntegrator::Rise() const
 {
-#ifdef ATOMIC_BLOCK
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
   {
-#endif
     return rise;
-#ifdef ATOMIC_BLOCK
   }
-#endif
 }
 
 bool DebouncerIntegrator::Fall() const
 {
-#ifdef ATOMIC_BLOCK
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
   {
-#endif
     return fall;
-#ifdef ATOMIC_BLOCK
   }
-#endif
 }
 
 #if DEBOUNCER_REPEAT_COUNT
 unsigned long DebouncerIntegrator::RepeatCount() const
 {
-#ifdef ATOMIC_BLOCK
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
   {
-#endif
     return repeat_count;
-#ifdef ATOMIC_BLOCK
   }
-#endif
 }
 #endif
 
@@ -102,16 +86,12 @@ unsigned long DebouncerIntegrator::RepeatCount() const
 void DebouncerIntegrator::Update()
 {
   // Temporarily disable interrupts to ensure an accurate time stamp for the sample, and that status flags are updated synchronously.
-#ifdef ATOMIC_BLOCK
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
   {
-#endif
     const bool input_state = digitalRead(INPUT_PIN);  // No interrupts will occur between here...
     const unsigned long current_ms = millis();        // ...and here, ensuring an accurate time stamp for the sample...
     Update_(input_state, current_ms);                 // ...and here, ensuring flags are updated synchronously.
-#ifdef ATOMIC_BLOCK
   }
-#endif
 }
 
 // For use as an Interrupt Service Routine where interrupts are disabled upon calling.
